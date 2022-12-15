5d4ce71022cc59007cce9a22699afb7d2876029b
Jorge Espindola <jlespindolaf@gmail.com>
2022-12-15 14:14:11 -0300

    Created and tested get notifications by recipient use case

--- src/application/use-cases/get-recipient-notifications.spec.ts
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -1,11 +1,11 @@
 import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository"
-import { CountRecipientNotifications } from "./count-recipient-notifications";
+import { GetRecipientNotifications } from "./get-recipient-notifications";
 
-describe('Count recipient notifications', () => {
-    it('should be able to count recipient notifications', async () => {
+describe('Get recipient notifications', () => {
+    it('should be able to get recipient notifications', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
-        const countNotification = new CountRecipientNotifications(notificationsRepository);
+        const getRecipientNotifications = new GetRecipientNotifications(notificationsRepository);
 
         await notificationsRepository.create(
             makeNotification({ recipientId: 'recipient-1' })
@@ -20,11 +20,15 @@ describe('Count recipient notifications', () => {
         );
 
         
-        const { count } = await countNotification.execute({
+        const { notifications } = await getRecipientNotifications.execute({
             recipientId: 'recipient-1'
         })
 
-        expect(count).toEqual(2);
+        expect(notifications).toHaveLength(2);
+        expect(notifications).toEqual(expect.arrayContaining([
+            expect.objectContaining({ recipientId: 'recipient-1' }),
+            expect.objectContaining({ recipientId: 'recipient-1' })
+        ]));
     })
     
 })
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -8,19 +8,19 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
     async findById(notificationId: string): Promise<Notification | null> {
         const notification = this.notifications.find(
             item => item.id === notificationId
-        )
+            )
         
-        if (!notification) {
-            return null
+            if (!notification) {
+                return null
+            }
+            
+            return notification
         }
         
-        return notification
-    }
-    
-    async create(notification: Notification) {
-        this.notifications.push(notification)
-    }
-    
+        async create(notification: Notification) {
+            this.notifications.push(notification)
+        }
+        
     async save(notification: Notification): Promise<void> {
         const notificationIndex = this.notifications.findIndex(
             item => item.id === notification.id
@@ -30,10 +30,16 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
             this.notifications[notificationIndex] = notification;
         }
     }
-
+    
     async countManyByRecipientId(recipientId: string): Promise<number> {
         return this.notifications.filter(
             notification => notification.recipientId === recipientId
         ).length
+    }
+        
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        return this.notifications.filter(
+            notification => notification.recipientId === recipientId
+        )
     }
 }
