7b4328c5c26fe39c8654aae49b65f1a25e05270b
Jorge Espindola <jlespindolaf@gmail.com>
2022-12-15 11:32:36 -0300

    Created and tested cancel notification use case

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Notification } from "@application/entities/notification";
+import { Content } from "@application/entities/notification-content";
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository"
+import { CancelNotification } from "./cancel-notification"
+import { NotificationNotFound } from "./errors/notification-not-found";
+
+describe('Cancel notification', () => {
+    it('should be able to cancel a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        const notification = new Notification({
+            content: new Content('Nova notificação'),
+            category: 'social',
+            recipientId: 'example-recipient-id',
+        })
+
+        await notificationsRepository.create(notification);
+
+        await cancelNotification.execute({
+            notificationId: notification.id
+        })
+
+        expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date))
+    })
+
+    it('should not be able to cancel a non existing notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fale-notification-id'
+            })
+        }).rejects.toThrow(NotificationNotFound)
+
+    }) 
+    
+})
--- src/infra/database/prisma/repositories/prisma.notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma.notifications.repository.ts
@@ -10,12 +10,20 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
         private prismaService: PrismaService
     ) {}
 
+    async findById(notificationId: string): Promise<Notification | null> {
+        throw new Error("Method not implemented.");
+    }
+    
     async create(notification: Notification): Promise<void> {
         const raw = PrismaNotificationMapper.toPrisma(notification);
-
+        
         await this.prismaService.notification.create({
             data: raw
         })
+    }
+
+    async save(notification: Notification): Promise<void> {
+        throw new Error("Method not implemented.");
     }
 
 }
